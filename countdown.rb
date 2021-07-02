890c6ddbca9476a539dd25c92358c1a9db2e93b8
sarwost <swostenberg@gmail.com>
2021-07-01 20:44:52 -0600

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    number = x
+    while number > 0 
+        puts "#{number} SECOND(S)!" 
+        number -= 1
+    end 
+    "HAPPY NEW YEAR!"
 end
