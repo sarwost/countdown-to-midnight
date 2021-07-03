bf04782c5a268c4424767911cea1261df26cf478
sarwost <swostenberg@gmail.com>
2021-07-03 14:29:47 -0600

    Done.

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(x)
     end 
     "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(x)
+    number = x
+    while number > 0 
+        puts "#{number} SECOND(S)!" 
+        number -= 1
+        sleep(2)
+    end 
+    "HAPPY NEW YEAR!"
+end
